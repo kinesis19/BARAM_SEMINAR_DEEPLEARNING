6794b1116e91f0a35b4a26c79ba64bc19cce7d8f
Kinesis <lordkinesis@naver.com>
2024-04-08 21:05:41 +0900

    Adding EdgeDetect.py

---
+++ EdgeDetect.py
@@ -0,0 +1,17 @@
+# 개발 환경 구성
+# pip install opencv-python
+# pip install numpy
+
+import cv2
+import numpy as np
+
+image = cv2.imread('/Users/lordk/Github/BARAM_SEMINAR_DEEPLEARNING/robot.jpg')
+
+horizontal_filter = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
+vertical_filter = np.array([[1, 0, -1], [1, 0, -1], [1, 0, -1]])
+
+horizontal_result = cv2.filter2D(image, -1, horizontal_filter)
+vertical_result = cv2.filter2D(image, -1, vertical_filter)
+
+cv2.imwrite('horizontal_pattern.jpg', horizontal_result)
+cv2.imwrite('vertical_pattern.jpg', vertical_result)
